486ab4c86e1943561a270a0e8b149c989eda8ffc
RyanDaCoder <ryancollins555@gmail.com>
2020-09-30 13:58:55 -0700

    add a post request sending score and player name to index.js

--- Game.js
+++ Game.js
@@ -252,13 +252,6 @@ async function draw () {
 	knife2.draw();
 	knife3.draw();
 	if (gameOver) {
-<<<<<<< HEAD
-		var url = new URL("https://tragically-goose-74465.herokuapp.com/test");
-		url.searchParams.append("rows", "3");
-		url.searchParams.append("offset", "0");
-		let response = await axios.get(url);
-		leaderboard = response.data;
-=======
 		if (leaderboard === null) {
 			var url = new URL("https://tragically-goose-74465.herokuapp.com/test");
 			url.searchParams.append("rows", "3");
@@ -268,7 +261,6 @@ async function draw () {
 		}
 		
 		
->>>>>>> 9c2b2e5817117147cd1cdc901305188b32baa8c0
 		knife.x = 5550;
 		knife.speed = 0;
 		knife1.x = 5550;
@@ -289,6 +281,10 @@ async function draw () {
 		
 		if (sendGameOver === false) {
 			sendGameOver = true;
+			let response = await axios.post("http://localhost:3001/leaderboard", {
+			 	"score": highscore,
+			 	"playerName": player
+			});
 		}
 	}	
 }
